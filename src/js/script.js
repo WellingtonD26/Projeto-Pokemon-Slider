2c31b3f6e608841d17da72c96be4218509913852
WellingtonD26 <110069926+WellingtonD26@users.noreply.github.com>
2022-08-08 19:20:50 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+// document.addEventListener("keydown", function()) {
+//     if(e.key == "Enter") {
+//         console.log ("fodase meu irmao")
+//     }
+// }
+
+// document.addEventListener("keypress", function(e)) {
+//     if(e.key === "Enter")
+// }
+
+// document.addEventListener("keypress")
+
+// document.onkeyup=function(_e){
+//     if
+// }
+
+// document.onkeyup=function(e){
+//     var e = e || window.event; // for IE to cover IEs window event-object
+//     if(e.altKey && e.which == 65) {
+//       //Chama sua função JS aqui
+//     }
+//   }
+  
